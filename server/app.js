844048f1f209569c81b92c062c7b63f285f1916d
chataolauj <chataolauj@gmail.com>
2020-07-02 21:48:41 -0400

    Adding conditions for deployment

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const session = require('express-session');
 const passport = require('passport');
 const db = require('./db/db');
+const path = require('path');
 
 const app = express();
 
@@ -44,6 +45,15 @@ app.use('', require('./routes/auth'));
 app.get('/', (req, res) => {
     res.send('This is home...');
 });
+
+if(process.env.NODE_ENV === 'production') {
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
