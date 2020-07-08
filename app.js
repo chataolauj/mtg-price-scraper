ab8cc75611ec88121f6052605ede7922efc99296
chataolauj <chataolauj@gmail.com>
2020-07-07 20:01:44 -0400

    Changed 'client/build' to 'client/dist'

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get('/', (req, res) => {
 
 if(process.env.NODE_ENV === 'production') {
     //Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('client/dist'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
