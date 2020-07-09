eb55785e28730de5b45a5b7f18a90a4a63bb35c0
chataolauj <chataolauj@gmail.com>
2020-07-09 02:25:59 -0400

    Updating scrape.js to work on Heroku.

--- src/scrape.js
+++ src/scrape.js
@@ -15,7 +15,12 @@ async function scrapeTCG(card_name, set_name) {
     }
 
     const url = `https://shop.tcgplayer.com/magic/${set_name.toLowerCase().replace(/:?,?\s+/g, "-")}/${card_name.toLowerCase().replace(/:?,?\s+/g, '-')}`;
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        'args' : [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ]
+    });
     const page = await browser.newPage();
     await page.setViewport({width: 1920, height: 1080});
     await page.goto(url, { waitUntil: 'networkidle2'});
